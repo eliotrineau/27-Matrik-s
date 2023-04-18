9c68ecf8598c732d58b22c855fa1061e0a85dff9
eliotrineau <eliot.rineau@edu.ece.fr>
2023-04-18 13:49:07 +0200

    test

--- animeJS.js
+++ animeJS.js
@@ -1,1 +1,21 @@
 import anime from 'animejs'
+
+anime({
+    targets: 'test',
+    translateX: {
+      value: 250,
+      duration: 800
+    },
+    rotate: {
+      value: 360,
+      duration: 1800,
+      easing: 'easeInOutSine'
+    },
+    scale: {
+      value: 2,
+      duration: 1600,
+      delay: 800,
+      easing: 'easeInOutQuart'
+    },
+    delay: 250 // All properties except 'scale' inherit 250ms delay
+  });
